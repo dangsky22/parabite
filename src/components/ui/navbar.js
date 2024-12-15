d310ae6303dfb961211e70816ed258397aca2785
RayhanAlsauqi <rayhan.alsauqi@sarmurni.co.id>
2024-12-16 01:42:40 +0700

    perbaikan animasi splash dan onboarding

--- src/components/ui/navbar.js
+++ src/components/ui/navbar.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Navbar(){
   return(
-    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md border-t border-gray-200 flex justify-around py-3">
+    <div className="max-w-[375px] mx-auto fixed bottom-0 left-0 right-0 bg-white shadow-md border-t border-gray-200 flex justify-around py-3">
       <Image src="/svg/navbar-home.svg" alt="icon navbar" width={32} height={32} />
       <Image src="/svg/navbar-message.svg" alt="icon navbar" width={32} height={32} />
       <Image src="/svg/navbar-order.svg" alt="icon navbar" width={32} height={32} />
