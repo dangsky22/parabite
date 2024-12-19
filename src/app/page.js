5fac0f4054012ab03e6472f23960c9e31bc8da1b
syahlaNPS <syahlanadya5@gmail.com>
2024-12-19 16:26:16 +0700

    sign up

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,16 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-   <motion.div
-   className="flex items-center justify-center h-screen bg-orange-100"
-   initial={{ opacity: 0, y: 50 }}
-   animate={{ opacity: 10, y: 50 }}
-   transition={{ duration: 0.5, delay:1.5 }}
-   >
-    <motion.div initial={{y: -50,opacity:0}} animate={{y:0,opacity:1}} transition={{duration:0.5,ease:"easeInOut"}}>
+    <motion.div
+      className="flex items-center justify-center h-screen bg-orange-100"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 10, y: 50 }}
+      transition={{ duration: 0.5, delay:1.5 }}
+    >
+      <motion.div initial={{y: -50,opacity:0}} animate={{y:0,opacity:1}} transition={{duration:0.5,ease:"easeInOut"}}>
 
-     <Image src="/svg/onboarding.svg" alt="onboarding" width={779} height={779} className="object-contain"/>
+        <Image src="/svg/onboarding.svg" alt="onboarding" width={779} height={779} className="object-contain"/>
+      </motion.div>
     </motion.div>
-   </motion.div>
   );
 }
