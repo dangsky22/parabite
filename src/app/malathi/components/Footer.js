f27577e6f981e49ed88af9d1329f0de132184d1b
lintangabst <lintang.suto@students.paramadina.ac.id>
2024-12-29 16:50:18 +0700

    add href for kantin page to pembayaran

--- src/app/malathi/components/Footer.js
+++ src/app/malathi/components/Footer.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer({ totalPrice }) {
   return (
     <footer className="fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-white border-1 border-gray-400 border-t-2 flex justify-center items-center w-[430px] h-[95px]">
+      <Link href="/pembayaran" passHref>
       <div className="bg-gradient-to-r from-orange-400 to-orange-500 p-4 shadow-2xl rounded-full w-[300px] h-auto flex items-center justify-between px-8">
         <Image
           src="/svg/cart.svg"
@@ -16,6 +18,7 @@ export default function Footer({ totalPrice }) {
           {totalPrice === 0 ? "--,--" : totalPrice.toLocaleString("id-ID")}
         </p>
       </div>
+      </Link>
     </footer>
   );
 }
